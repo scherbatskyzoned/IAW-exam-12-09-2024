91e35d738cca7551ae39def390f3633106182c11
Beatrice <s281129@studenti.polito.it>
2024-08-31 19:12:07 +0200

    aggiunto check password sicura

--- static/js/checkPassword.js
+++ static/js/checkPassword.js
@@ -2,9 +2,23 @@ function checkPassword(e) {
   let password = document.getElementById('passwordInput').value;
   let checkPassword = document.getElementById('passwordCheckInput').value;
   let errorMessage = document.getElementById('error-message');
+  let upperCaseLetters = /[A-Z]/g;
+  let lowerCaseLetters = /[a-z]/g;
+  let numbers = /[0-9]/g;
 
-  console.log(password, checkPassword);
-  if (password != checkPassword) {
+
+  if (password.length < 8) {
+    errorMessage.textContent = "La password deve essere di almeno 8 caratteri."
+    errorMessage.style.color = "#b3261e";
+    e.preventDefault();
+  }
+  else if (!password.match(upperCaseLetters) || !password.match(lowerCaseLetters) ||
+           !password.match(numbers)) {
+    errorMessage.textContent = "Scegli una password più sicura. Prova a utilizzare una combinazione di lettere e numeri."
+    errorMessage.style.color = "#b3261e";
+    e.preventDefault();
+  }
+  else if (password != checkPassword) {
     errorMessage.textContent = "Le password non corrispondono. Riprova.";
     errorMessage.style.color = "#b3261e";   
     e.preventDefault();
